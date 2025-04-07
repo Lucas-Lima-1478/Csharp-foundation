b9851eba18208e1caa9fbe9b8b8ee215b9aeaceb
Lucas-Lima-1478 <lucas.lima.1478@outlook.com>
2025-04-07 20:57:52 -0300

    feat: use Person class with C# CamelCase convention in variable name

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharp_foundation.Models;
+
+Person person = new Person();
+person.Name = "Lucas Lima";
+person.Age = 25;
+person.PersonPresent();
